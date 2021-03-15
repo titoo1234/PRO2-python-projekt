dbb5bd752cb5dff950931086031474cdf6584f5a
titoo1234 <49947010+titoo1234@users.noreply.github.com>
2021-03-15 20:33:33 +0100

    Add files via upload

--- orodja.py
+++ orodja.py
@@ -1,6 +1,6 @@
 #koda za pridobitev imen za nek seštevek
 #treba še mal uredit imena 
-
+link = 'https://www.procyclingstats.com/race/tour-de-france/2020'
 import requests
 import re
 def imena_vseh(link):
@@ -26,7 +26,25 @@ def imena_vseh(link):
             
         
     return slovar
-
+def imena_etapa(link):
+    '''vrne seznam imen kolesarjev glede na posamezne razvrstitve'''
+    # dat2 = open('dat2.txt','w',encoding='utf-8')
+    
+    req = requests.get(link).text
+    
+    tabela = req.split('<table class="basic')
+    #tuki sam spremeni indeks in boš dobu imena za drugo
+    #točkovanje(2=GC,3=ZELENA,4=PIKČASTA) a
+                                                    
+    for i in [1]:#4 je bela majica lahk dodama...
+        tab = tabela[i]
+        osebe = re.findall(r'href="rider/.+">.+class="showIfMobile', tab)
+        imena = [re.sub(r'href="rider/', '', oseba) for oseba in osebe]
+        imena = [uredi_ime(oseba.split('"')[0]) for oseba in imena]
+        
+            
+        
+    return imena
 
 def uredi_ime(niz):
     '''uredi ime oblike ime-priimek v obliko Ime priimek'''
@@ -47,5 +65,20 @@ def pridobivanje_vseh_let(link, od_leta): #link = https://www.procyclingstats.co
     for leto in range(od_leta,2021):
         slovar[leto] = imena_vseh(link + '/'+str(leto))
     return slovar
-        
-        
+
+def naslov_etap(link):
+    '''vrne tabelo kratic etap, da bom jih lahko uporabu za funkcijo etape'''
+    tab = []
+    req = requests.get(link).text
+    tekme = re.findall(r'max-width: 250px; width: 100%;  "><select style="" onChange="window.location.href=this.value;.+">.+</option><option', req)
+    
+    return tekme
+def etape(link,leto):
+    '''vrne slovar, ključi so katera etapa, vrednosti pa seznam kolesarjev'''
+    slovar = dict()
+    for etapa in range(1,22):
+        slovar[str(etapa)+'. etapa']= imena_etapa(link+ '/'+str(leto) +'/'+'stage-'+str(etapa) + '/'+'result'+'/'+'result')
+    return slovar
+    
+      
+print(naslov_etap(link))
