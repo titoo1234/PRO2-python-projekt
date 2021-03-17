05ccc8becd04827f2f1c0911062cd675d71e530f
titoo1234 <tito.arnsek@gmail.com>
2021-03-17 18:59:42 +0100

    p

--- branje_txt.py
+++ branje_txt.py
@@ -1,5 +1,9 @@
+#import razred
 import ast
 dat = open('dat.txt','r')
 besedilo = dat.read()
-slovar = ast.literal_eval(besedilo)
+slovar_vseh_let = ast.literal_eval(besedilo)
 dat.close()
+for leto in slovar_vseh_let:
+    print(leto)
+    
