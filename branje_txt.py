390bbf6a56e0d797cae39dfd4c7a92b82b7c0742
titoo1234 <tito.arnsek@gmail.com>
2021-03-17 19:48:39 +0100

    asd

--- branje_txt.py
+++ branje_txt.py
@@ -4,6 +4,14 @@ dat = open('dat.txt','r')
 besedilo = dat.read()
 slovar_vseh_let = ast.literal_eval(besedilo)
 dat.close()
+mn_kolesarjev = set()
 for leto in slovar_vseh_let:
-    print(leto)
+    for etapa in slovar_vseh_let[leto]['ETAPE']:
+        for kolesar in slovar_vseh_let[leto]['ETAPE'][etapa]:
+            if kolesar in mn_kolesarjev:
+                continue
+            else:
+                #naredimo kolesarja
+                mn_kolesarjev.add(kolesar)
+                
     
