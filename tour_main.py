68024e4fb73956e245d2553d71191ced7a421163
titoo1234 <tito.arnsek@gmail.com>
2021-03-29 09:18:29 +0200

    spremenmba imena datoteke

--- tour_main.py
+++ tour_main.py
@@ -5,14 +5,14 @@ import pickle
 link = 'https://www.procyclingstats.com/race/tour-de-france'
 datoteka = 'vse.txt'
 
-# slovar_imen = branje_txt.branje_ustvarjanje(datoteka)
+slovar_imen = branje_txt.branje_ustvarjanje(datoteka)
 # slovar_objektov_brez = branje_txt.ustvarjanje_objektov(slovar_imen)
 # slovar_objektov =  branje_txt.zamenjava_kljucev(slovar_objektov_brez)
 
   
 # #Step 1
 # with open('slovar_objektov_brez_datoteka', 'wb') as datoteka:
-#     pickle.dump(slovar_objektov_brez, datoteka)
+#     pickle.dump(slovar_objektov_ne_prava_imena, datoteka)
 
 # with open('slovar_objektov_datoteka', 'wb') as datoteka:
 #     pickle.dump(slovar_objektov, datoteka)
@@ -21,7 +21,7 @@ datoteka = 'vse.txt'
 
 
 # #step 2
-with open('slovar_objektov_brez_datoteka', 'rb') as datoteka:
+with open('slovar_objektov_ne_prava_imena', 'rb') as datoteka:
     slovar_kolesarjev_brez_pravih_imen = pickle.load(datoteka)
  
 with open('slovar_objektov_datoteka', 'rb') as datoteka:
